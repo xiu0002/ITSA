4dc40ef6ef5144eff40a818aeffa346c62596051
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-09 23:05:44 +0800

    Add files via upload

---
+++ ITSA3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+	int r = 100, h;
+	h = sqrt((x * x) + (y * y));
+
+	if (h < r) {
+		cout << "inside" << endl;
+	}
+	else {
+		cout << "outside" << endl;
+	}
+	return 0;
+}
