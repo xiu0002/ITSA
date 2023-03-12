ccf376a703c6ada3ce8ccd16602d4daee231b007
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-12 19:17:04 +0800

    Add files via upload

---
+++ ITSA6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int month; 
+	
+	cin >> month;
+
+	if (month >= 3 && month <= 5) 
+		cout << "Spring"<< endl;
+	else if (month >= 6 && month <= 8)
+		cout <<"Summer" << endl;
+	else if (month >= 9 && month <= 11)
+		cout << "Autumn" << endl;
+	else 
+		cout << "Winter" << endl;
+
+	return 0;
+}
+
