e5456d6456701cc8e7a0c9726135b06ab1e6f074
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-13 23:08:29 +0800

    Add files via upload

---
+++ ITSA5.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>  
+using namespace std;
+
+int main() {
+    int x = 0 , a[8] , i;
+
+    cin >> x;
+    if (x < 0)
+    {
+        x += 256;  
+    }
+    for (i = 7; i >= 0; i--)
+    {
+        a[i] = x % 2;
+        x /= 2;
+    }
+    for (i = 0; i < 8; i++)
+    {
+        cout << a[i];
+    }
+         cout << endl;
+
+    return 0;
+}
