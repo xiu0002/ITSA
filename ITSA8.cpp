9cd62297f2b5e65bb3ff786cd99adbd4d9635304
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-20 15:16:46 +0800

    Add files via upload

---
+++ ITSA8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin >> x;
+
+    for (y = 2; y < x; y++) {
+        if (x % y == 0) {
+            break;
+        }
+    }
+        if (y == x) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    
+    return 0;
+}
+
