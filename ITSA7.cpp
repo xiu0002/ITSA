745869f9cce642e955c4458e48b0afec1d142650
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-20 15:07:54 +0800

    Add files via upload

---
+++ ITSA7.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    char w;
+    cin >> n;
+    while (n--)
+    {
+        int a1, a2, b1, b2;
+       
+        cin >> w >> a1 >> a2 >> b1 >> b2;
+        switch (w) {
+        case '+':cout << a1 + b1 << " " << a2 + b2 << endl;
+            break;
+        case '-':cout << a1 - b1 << " " << a2 - b2 << endl; 
+            break;
+        case '*':cout << a1 * b1 - a2 * b2 << " " << a2 * b1 + a1 * b2 << endl; 
+            break;
+        case '/':cout << (a1 * b1 + a2 * b2) / (b1 * b1 + b2 * b2) << " " << (a2 * b1 - a1 * b2) / (b1 * b1 + b2 * b2) << endl;
+            break;
+        }
+    }
+
+}
