ead30294a305524886fdcdc56f4b3714e322b1ed
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-20 15:17:28 +0800

    Add files via upload

---
+++ ITSA10.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h> 
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+
+    while ((a %= b) && (b %= a));
+        cout << a + b << endl;
+
+    return 0;
+}
+
