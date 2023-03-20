0b2d6a783f9cc5822673fb1c5568df64bf17f334
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-20 21:42:10 +0800

    Add files via upload

---
+++ itsa14.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	int a, b;
+	string str;
+	cin >> str;
+	
+		b = 1;
+		a = str.length();
+		for (int i = 0; i < a / 2; i++)
+		{
+			if (str[i] != str[a - i - 1])
+			{
+				b = 0;
+				break;
+			}
+		}
+		if (b == 1)
+		{
+			cout << "YES" << endl;
+		}
+		else
+		{
+			cout << "NO" << endl;
+		}
+		
+	return 0;
+}
