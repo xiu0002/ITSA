75a88a5b179a1eca772a850200ee909094192bee
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-20 15:17:47 +0800

    Add files via upload

---
+++ ITSA11.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, count = 0;
+    cin >> a >> b;
+    int box[a * b];
+    for (int i = 0; i < (a * b); i++) {
+        cin >> box[i];
+    }
+    for (int y = 0; y < b; y++) {
+        for (int x = 0; x < a; x++) {
+            count++;
+            cout << box[x * b + y];
+            if (count % a != 0) {
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
