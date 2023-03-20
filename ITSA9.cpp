0b4675ab7563966e9b1db4b42458e6afe3da5f56
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-20 15:17:07 +0800

    Add files via upload

---
+++ ITSA9.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int n,i,sum = 0;
+	cin >> n;
+	for (i = 1; i <= n; i++) {
+		if (i % 3 == 0) {
+			sum += i;
+		}
+	}
+	cout << sum << endl;
+	return 0;
+}
+
