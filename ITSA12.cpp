3edfde578b6269dd867274b4fe8e8b930f8778e8
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-03-20 15:18:08 +0800

    Add files via upload

---
+++ ITSA12.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int f(int n) {
+	if (n == 0 || n == 1) {
+		return n+1;
+	}
+	else if (n > 1) {
+		return f(n - 1) + f(n / 2);
+	}
+}
+int main()
+{
+	int n;
+	while (cin >> n) {
+		cout << f(n) << endl;
+	}
+	return 0;
+}
+
+
