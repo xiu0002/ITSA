167f465498a5a093ee2605f880a40d841ba356f3
xiu0002 <127383119+xiu0002@users.noreply.github.com>
2023-04-16 20:01:28 +0800

    Add files via upload

---
+++ ITSA23.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<sstream>
+using namespace std;
+int main() {
+	string input, str;
+	int mon[4];
+
+	getline(cin, input);
+	istringstream cut(input);
+	for (int i = 0; i < 4; i++)
+	{
+		getline(cut, str, ',');
+		istringstream sa(str);
+		sa >> mon[i];
+	}
+	if (mon[0] > mon[1] * 15 + mon[2] * 20 + mon[3] * 30) 
+	{
+		mon[0] = mon[0] - mon[1] * 15 - mon[2] * 20 - mon[3] * 30; 
+		mon[2] = mon[0] / 50; mon[0] %= 50;
+		mon[1] = mon[0] / 5; mon[0] %= 5;
+		cout << mon[0] << "," << mon[1] << "," << mon[2] << endl; return 0;
+	}
+	else cout << "0" << endl; return 0;
+}
